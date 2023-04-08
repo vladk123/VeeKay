305c67150672531dc1d78dfc8f14f14e3cbae960
Vlad <vlad.kapitantchouk@gmail.com>
2023-04-08 16:57:28 -0400

    Add Basic Form Spam Filtering Question
    Ask how many fingers are on a human's left hand, to help filter spam a little.

--- controllers/main.js
+++ controllers/main.js
@@ -1,9 +1,17 @@
 const sendEmail = require('../utils/send_email');
 
 module.exports.sendEmail = async(req, res, next) => {
-    // send email
-    const {contactName, contactEmail, contactMessage} = req.body
-    await sendEmail(undefined,undefined,`New Veekay Message`, "email.ejs", {contactName, contactEmail, contactMessage})
+    
+    const {contactName, contactEmail, contactMessage, contactConfirm} = req.body
+
+    // If not all fields are filled out or answer is incorrect
+    if(!contactName || !contactEmail || !contactMessage || !contactConfirm || contactConfirm !== 'five' || contactConfirm !== 'Five' || contactConfirm !== '5'){
+        console.log('Spam...')
+        return res.redirect('/?submitted=true');
+    }
+
+    // Send email
+    await sendEmail(undefined,undefined,`New Veekay Message`, "email.ejs", {contactName, contactEmail, contactMessage, contactConfirm})
 
     return res.redirect('/?submitted=true');
 }
